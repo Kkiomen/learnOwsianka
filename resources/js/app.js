f0d32c191978d33cd6eb537743aba97f34b3795e
Jakub Owsianka <j.owsianka@sente.pl>
2023-12-15 21:15:52 +0100

    add login panel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,7 @@
 import './bootstrap';
-import { Livewire } from '../../vendor/livewire/livewire/dist/livewire.esm'
 
-Livewire.start()
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
