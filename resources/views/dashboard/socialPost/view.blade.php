0d55ac5ba07f493a090dc31eb92befa715b706e3
Jakub Owsianka <j.owsianka@sente.pl>
2024-01-02 17:42:03 +0100

    Save image

--- resources/views/dashboard/socialPost/view.blade.php
+++ resources/views/dashboard/socialPost/view.blade.php
@@ -113,7 +113,11 @@
                                        Save
                                    </button>
 
-                                   <a href="{{ route('blogPost', ['slug' => $blog->slug]) }}" target="_blank">
+                                   @if($blog->language == 'pl')
+                                    <a href="{{ route('blogPost', ['slug' => $blog->slug]) }}" target="_blank">
+                                   @else
+                                            <a href="https://oatllo.pl/article/ {{ $blog->slug }}" target="_blank">
+                                   @endif
                                        <button type="button"
                                                class="inline-flex items-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
                                            <i class="fa-solid fa-eye mr-2"></i>
